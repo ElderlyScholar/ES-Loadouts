b1c2d229fe24cf0b0706b89b7009ef81965fcdf2
ElderlyScholar <pinewoodstaterp@gmail.com>
2024-05-24 08:27:32 +1000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,31 @@
+Config = {}
+
+Config.PDCrateItem = "pd_loadout_crate"
+Config.EMSCrateItem = "ems_loadout_crate"
+
+Config.PoliceJobName = "police"
+Config.EMSJobName = "ambulance"
+
+Config.Loadouts = {
+    PD = {
+        [1] = {
+            item = "weapon_pistol",
+            amount = 1,
+        },
+        [2] = {
+            item = "radio",
+            amount = 2,
+        },
+    },
+    
+    EMS = {
+        [1] = {
+            item = "",
+            amount = 1,
+        },
+        [2] = {
+            item = "",
+            amount = 1,
+        },
+    },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Noneya Business'
+description 'A loadout system designed for qb-core, so police can grab a crate and be done'
+version '1.0.0'
+
+server_scripts {
+    'server/*.lua',
+}
+
+shared_scripts {
+    'config.lua',
+}
